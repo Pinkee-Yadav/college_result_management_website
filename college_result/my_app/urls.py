32bcbd9a1b72ff68b66a8b7ad82b5d42c9d5bd33
Pinkee Yadav <pinkeeyadav7006@gmail.com>
2025-07-20 17:02:37 +0545

    first commit

---
+++ college_result/my_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from my_app import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register-student/', views.register_student, name='register_student'),
+    path('register-teacher/', views.register_teacher, name='register_teacher'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('teacher-dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('student-dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('add_result/', views.add_result, name='add_result'),
+    path('view-results/', views.view_results, name='view_results'),
+     
+]
